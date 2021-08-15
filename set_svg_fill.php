6c64733f879c664e8f2680d0cca98fcf157b2cb0
Forecaster <forecaster@towerofawesome.org>
2021-08-15 08:23:35 +0200

    - Add some output to svg color replacer - Url encode # in css

--- set_svg_fill.php
+++ set_svg_fill.php
@@ -1,15 +1,15 @@
 <?php
 if (!isset($_GET['path']))
-	die();
+	die("No path");
 $path = $_GET['path'];
 header("Content-type: image/svg+xml;");
 
 $svg = file_get_contents(__DIR__ . "/" . $path);
 if (!$svg)
-	die();
+	die("File not found");
 if (isset($_GET['fill'])) {
 	$re = '/(fill=\").*?(\")/m';
 	$subst = '$1' . $_GET['fill'] . '$2';
-
 	die(preg_replace($re, $subst, $svg));
 }
+die($svg);
